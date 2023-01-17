996bc2a6a4bafa4a7c66cae5c85c75024b22a528
ipekurunn <ipekurun.96@hotmail.com>
2023-01-17 13:59:09 +0300

    Proje dosyası ekle.

---
+++ Exercise14_ForAndForeachLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ConsoleCards;
+namespace Exercise14_ForAndForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem1 Create objects
+            Deck deck = new Deck();
+            Card[] hand = new Card[5];
+            deck.Shuffle();
+
+            for (int i = 0; i < 5; i++)
+            {
+                hand[i] = deck.TakeTopCard();
+                hand[i].FlipOver();
+                hand[i].Print();
+            }
+
+        }
+    }
+}
